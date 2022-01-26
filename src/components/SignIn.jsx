251549f666e438e501aa050d066dad8f87e2ff29
cedricquitor <cedricquitor.dev@gmail.com>
2022-01-26 09:20:17 +0800

    Fixed login carousel & added comments

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -5,37 +5,45 @@ import { signInWithGoogle } from "../firebase/utils";
 import data from "../test/data";
 
 const SignIn = () => {
+  // State handler for index and assigning data to products.
   const [products, setProducts] = useState(data);
   const [index, setIndex] = useState(0);
 
+  // UseEffect hook that solves the slider's caveat.
   useEffect(() => {
     const lastIndex = products.length - 1;
+    // Set index to the last index if it is less than 0.
     if (index < 0) {
       setIndex(lastIndex);
     }
+    // Set index to 0 if it is on the last index.
     if (index > lastIndex) {
       setIndex(0);
     }
   }, [index]);
 
+  // UseEffect hook that slides the image every 4 seconds.
   useEffect(() => {
     let slider = setInterval(() => {
       setIndex(index + 1);
-    }, 4000);
+    }, 8000);
     return () => clearInterval(slider);
   }, [index]);
 
+  // Form submit will not refresh the page.
   const handleSubmit = async (e) => {
     e.preventDefault();
   };
 
   return (
-    <div className="relative mt-12 bg-white w-3/4 m-auto drop-shadow-xl">
+    <div className="relative mt-12 bg-white w-5/6 m-auto drop-shadow-xl">
       <div className="grid grid-cols-2">
-        <div className="bg-red-200 flex flex-row justify-center items-center overflow-hidden">
+        {/* Left Side Grid */}
+        <div className="bg-sky-100 flex flex-row justify-center items-center overflow-hidden">
+          {/* Map the products from the data source. Giving each product an index.*/}
           {products.map((product, productIndex) => {
+            // Deconstructing product name and its thumbnail from the product array.
             const { productName, productThumbnail } = product;
-
             let position = "slider__img--nextSlide";
             if (productIndex === index) {
               position = "slider__img--activeSlide";
@@ -43,17 +51,15 @@ const SignIn = () => {
             if (productIndex === index - 1 || (index === 0 && productIndex === products.length - 1)) {
               position = "slider__img--lastSlide";
             }
-
+            // For each product, return a div.
             return (
               <div className={position + " absolute"} key={productName}>
-                <img src={productThumbnail} alt={productName} className="object-cover" width="580px" />
+                <img src={productThumbnail} alt={productName} className="object-cover h-[40.4rem] w-full" width="650px" />
               </div>
             );
           })}
-          {/* <div className="bg-blue-200 flex items-center justify-center overflow-hidden max-h- aspect-h-4 aspect-w-3">
-            <img src={products[1].productThumbnail} className="object-cover" />
-          </div> */}
         </div>
+        {/* Right Side Grid */}
         <div className="mx-auto text-center my-16">
           <p className="text-gray-400 text-sm font-lato uppercase tracking-widest">Be a part of our story. Be a part of the brand.</p>
           <h1 className="text-5xl text-gray-900 font-playfair mb-8">
@@ -81,7 +87,7 @@ const SignIn = () => {
                 Login
               </a>
               <p className="font-playfair text-base font-medium text-left mb-2">Connect with Socials</p>
-              <button onClick={signInWithGoogle} className="w-full flex items-center justify-center px-8 py-3 outline outline-2 outline-sky-300 text-base font-medium shadow-md text-sky-300 bg-tranparent transition duration-300 hover:bg-sky-200 hover:text-white hover:shadow-2xl md:mb-4 md:py-3 md:text-lg md:px-10 focus:ring-2 focus:ring-offset-4 focus:ring-sky-300 active:bg-sky-600">
+              <button onClick={signInWithGoogle} className="w-full flex items-center justify-center px-8 py-3 outline outline-2 outline-sky-300 text-base font-medium shadow-md text-sky-300 bg-tranparent transition duration-300 hover:bg-sky-200 hover:text-white hover:shadow-2xl md:mb-4 md:py-3 md:text-lg md:px-10 focus:ring-2 focus:ring-offset-4 focus:ring-sky-200 active:bg-sky-600">
                 <FcGoogle className="mr-4 my-auto" />
                 Sign in with Google
               </button>
