a04f29bae3bda6770bb294d16e63a81191f1ab7c
cedricquitor <cedricquitor.dev@gmail.com>
2022-02-10 08:26:09 +0800

    Added links and animation to hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useThemeContext } from "../context/ThemeContext";
+import { FiHome } from "react-icons/fi";
+import { BsPatchQuestion } from "react-icons/bs";
+import { HiOutlineChatAlt2 } from "react-icons/hi";
+import { MdOutlineStorefront } from "react-icons/md";
 
 const Navbar = () => {
   // State handler to check if the page is scrolled.
@@ -67,17 +71,17 @@ const Navbar = () => {
         </div>
         <div className="nav__itemContainer lg:mr-8 2xl:mr-0">
           {colorTheme === "light" ? (
-            <span onClick={() => setTheme(colorTheme)}>
+            <button onClick={() => setTheme(colorTheme)}>
               <svg xmlns="http://www.w3.org/2000/svg" className={isScrolled ? "nav__item--secondary h-4 w-4" : "nav__item--secondary h-6 w-6"} fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
               </svg>
-            </span>
+            </button>
           ) : (
-            <span onClick={() => setTheme(colorTheme)}>
+            <button onClick={() => setTheme(colorTheme)}>
               <svg xmlns="http://www.w3.org/2000/svg" className={isScrolled ? "nav__item--secondary h-4 w-4" : "nav__item--secondary h-6 w-6"} fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
               </svg>
-            </span>
+            </button>
           )}
           <Link to="/shop">
             <svg xmlns="http://www.w3.org/2000/svg" className={isScrolled ? "nav__item--secondary h-4 w-4" : "nav__item--secondary h-6 w-6"} fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -106,17 +110,51 @@ const Navbar = () => {
       >
         <ul className="font-lato text-sm md:text-base text-gray-600" onClick={() => setIsClicked(false)}>
           <Link to="/" className="block px-5 py-2 mt-4 mb-2 md:px-10">
-            Home
+            <li className="flex">
+              <FiHome className="w-5 h-5 my-auto" />
+              <p className="my-auto ml-3">Home</p>
+            </li>
           </Link>
           <Link to="/shop" className="block px-5 py-2 mb-2 md:px-10">
-            Shop
+            <li className="flex">
+              <MdOutlineStorefront className="w-5 h-5 my-auto" />
+              <p className="my-auto ml-3">Shop</p>
+            </li>
           </Link>
           <Link to="/about" className="block px-5 py-2 mb-2 md:px-10">
-            About
+            <li className="flex">
+              <BsPatchQuestion className="w-5 h-5 my-auto" />
+              <p className="my-auto ml-3">About</p>
+            </li>
           </Link>
-          <Link to="/contact" className="block px-5 py-2 mb-4 md:px-10">
-            Contact
+          <Link to="/contact" className="block px-5 py-2 mb-2 md:px-10">
+            <li className="flex">
+              <HiOutlineChatAlt2 className="w-5 h-5 my-auto" />
+              <p className="my-auto ml-3">Contact</p>
+            </li>
           </Link>
+          <Link to="/login" className="block px-5 py-2 mb-2 md:px-10">
+            <li className="flex">
+              <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 my-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+              </svg>
+              <p className="my-auto ml-3">Account</p>
+            </li>
+          </Link>
+          <button onClick={() => setTheme(colorTheme)} className="block px-5 py-2 mb-4 md:px-10">
+            <li className="flex">
+              {colorTheme === "light" ? (
+                <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 my-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+                </svg>
+              ) : (
+                <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 my-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+                </svg>
+              )}
+              <p className="my-auto ml-3">{colorTheme === "light" ? "Dark Theme" : "Light Theme"}</p>
+            </li>
+          </button>
         </ul>
       </div>
     </nav>
