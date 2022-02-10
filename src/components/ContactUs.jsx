438a6ac3aba5c17863173bc8323a8cf285e27964
cedricquitor <cedricquitor.dev@gmail.com>
2022-02-10 08:44:15 +0800

    Added dark mode to about, contact, and shop

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -7,79 +7,80 @@ import { BsQuestionSquare } from "react-icons/bs";
 const ContactUs = () => {
   return (
     <div className="mt-12 mx-auto container">
-      <div className="pb-14 text-center border-b-[1px] border-gray-300">
-        <p className="text-gray-400 text-sm font-lato uppercase tracking-widest">We expect to hear from you soon.</p>
-        <h1 className="font-playfair font-bold text-5xl md:text-7xl lg:text-8xl tracking-tight text-gray-900">Contact Us</h1>
+      <div className="pb-14 text-center border-b-[1px] border-gray-500 dark:border-gray-400">
+        <p className="text-gray-500 text-sm font-lato uppercase tracking-widest dark:text-gray-400">We expect to hear from you soon.</p>
+        <h1 className="font-playfair font-bold text-5xl md:text-7xl lg:text-8xl tracking-tight text-gray-900 dark:text-zinc-100">Contact Us</h1>
       </div>
       <div className="mt-8 w-5/6 mx-auto">
-        <p className="mt-4 font-lato text-gray-500 text-xl">
+        <p className="mt-4 font-lato text-gray-500 text-xl dark:text-gray-400">
           For questions and concerns, please leave a message on our
           <a
             href="https://www.facebook.com/messages/t/218950794836813"
             target="_new"
-            className="ml-1 font-medium text-sky-300 transition border-b-[1px] border-sky-300 hover:text-sky-500 hover:border-sky-500 active:text-sky-600 active:border-sky-600"
+            className="ml-1 font-medium text-sky-300 transition border-b-[1px] border-sky-300 hover:text-sky-500 hover:border-sky-500 active:text-sky-600 active:border-sky-600 dark:text-sky-500 dark:border-sky-500"
           >
             Messenger
           </a>
           .
         </p>
-        <p className="font-lato text-gray-500 text-xl">
-          We'll be glad to address your concerns and queries between <span className="font-medium text-sky-300">8:00AM and 10:00PM</span>, from <span className="font-medium text-sky-300">Mondays through Saturdays.</span>
+        <p className="font-lato text-gray-500 text-xl dark:text-gray-400">
+          We'll be glad to address your concerns and queries between <span className="font-medium text-sky-300 dark:text-sky-500">8:00AM and 10:00PM</span>, from{" "}
+          <span className="font-medium text-sky-300 dark:text-sky-500">Mondays through Saturdays.</span>
         </p>
-        <p className="mt-12 font-lato text-gray-500 text-xl">Alternatively, you could also reach us at:</p>
+        <p className="mt-12 font-lato text-gray-500 text-xl dark:text-gray-400">Alternatively, you could also reach us at:</p>
         <div className="mt-8 grid gap-24 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
           <div>
             <div className="flex">
-              <FiPhoneCall className="w-6 h-6 mr-4 my-auto text-sky-300" />
-              <h2 className="font-lato font-semibold text-lg text-gray-900 uppercase">Call Us</h2>
+              <FiPhoneCall className="w-6 h-6 mr-4 my-auto text-sky-300 dark:text-sky-500" />
+              <h2 className="font-lato font-semibold text-lg text-gray-900 uppercase dark:text-zinc-100">Call Us</h2>
             </div>
-            <p className="font-lato text-gray-500 ">
+            <p className="font-lato text-gray-500 dark:text-gray-400">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit delectus laudantium nisi laborum similique, animi cum fugiat exercitationem eos reiciendis non incidunt quisquam repudiandae laboriosam, consequatur eius maiores facilis
               harum!
             </p>
           </div>
           <div>
             <div className="flex">
-              <HiOutlineMail className="w-6 h-6 mr-4 my-auto text-sky-300" />
-              <h2 className="font-lato font-semibold text-lg text-gray-900 uppercase">Email Us</h2>
+              <HiOutlineMail className="w-6 h-6 mr-4 my-auto text-sky-300 dark:text-sky-500" />
+              <h2 className="font-lato font-semibold text-lg text-gray-900 uppercase dark:text-zinc-100">Email Us</h2>
             </div>
-            <p className="font-lato text-gray-500">
+            <p className="font-lato text-gray-500 dark:text-gray-400">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat fuga ut iure porro! Consectetur accusamus, ut doloremque in eveniet sed dignissimos enim unde. Deleniti non magnam saepe quod veritatis quasi?
             </p>
           </div>
           <div>
             <div className="flex">
-              <BiBuildings className="w-6 h-6 mr-4 my-auto text-sky-300" />
-              <h2 className="font-lato font-semibold text-lg text-gray-900 uppercase">Visit Us</h2>
+              <BiBuildings className="w-6 h-6 mr-4 my-auto text-sky-300 dark:text-sky-500" />
+              <h2 className="font-lato font-semibold text-lg text-gray-900 uppercase dark:text-zinc-100">Visit Us</h2>
             </div>
-            <p className="font-lato text-gray-500">
+            <p className="font-lato text-gray-500 dark:text-gray-400">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore qui at culpa corrupti a odit eius rerum id sunt ducimus laboriosam, modi quas atque repudiandae consequatur voluptatibus illum incidunt molestiae.
             </p>
           </div>
           <div>
             <div className="flex">
-              <HiOutlineChatAlt2 className="w-6 h-6 mr-4 my-auto text-sky-300" />
-              <h2 className="font-lato font-semibold text-lg text-gray-900 uppercase">Live Chat</h2>
+              <HiOutlineChatAlt2 className="w-6 h-6 mr-4 my-auto text-sky-300 dark:text-sky-500" />
+              <h2 className="font-lato font-semibold text-lg text-gray-900 uppercase dark:text-zinc-100">Live Chat</h2>
             </div>
-            <p className="font-lato text-gray-500">
+            <p className="font-lato text-gray-500 dark:text-gray-400">
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam, libero possimus quae laborum ex facere temporibus tenetur ullam hic! Eaque labore corrupti quod dicta cupiditate vero voluptatibus. Deserunt, itaque in.
             </p>
           </div>
           <div>
             <div className="flex">
-              <BiBookBookmark className="w-6 h-6 mr-4 my-auto text-sky-300" />
-              <h2 className="font-lato font-semibold text-lg text-gray-900 uppercase">Appointments</h2>
+              <BiBookBookmark className="w-6 h-6 mr-4 my-auto text-sky-300 dark:text-sky-500" />
+              <h2 className="font-lato font-semibold text-lg text-gray-900 uppercase dark:text-zinc-100">Appointments</h2>
             </div>
-            <p className="font-lato text-gray-500">
+            <p className="font-lato text-gray-500 dark:text-gray-400">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae eligendi excepturi deleniti architecto quae soluta, assumenda quidem ipsum distinctio iure officia repellendus nemo delectus omnis id itaque hic, vero incidunt!
             </p>
           </div>
           <div>
             <div className="flex">
-              <BsQuestionSquare className="w-6 h-6 mr-4 my-auto text-sky-300" />
-              <h2 className="font-lato font-semibold text-lg text-gray-900 uppercase">FAQ's</h2>
+              <BsQuestionSquare className="w-6 h-6 mr-4 my-auto text-sky-300 dark:text-sky-500" />
+              <h2 className="font-lato font-semibold text-lg text-gray-900 uppercase dark:text-zinc-100">FAQ's</h2>
             </div>
-            <p className="font-lato text-gray-500">
+            <p className="font-lato text-gray-500 dark:text-gray-400">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui minus laboriosam delectus consequuntur excepturi quia libero, placeat assumenda veniam, recusandae cum laborum beatae voluptatibus in ducimus possimus odio quas totam.
             </p>
           </div>
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -32,17 +32,17 @@ const Products = () => {
   return (
     <div className="mt-12 container mx-auto">
       <div className="text-center">
-        <p className="text-gray-400 text-sm font-lato uppercase tracking-widest">Choose the one that fits your style.</p>
-        <h1 className="font-playfair font-bold text-5xl md:text-7xl lg:text-8xl tracking-tight text-gray-900">Our Jewelries</h1>
+        <p className="text-gray-500 text-sm font-lato uppercase tracking-widest dark:text-gray-400">Choose the one that fits your style.</p>
+        <h1 className="font-playfair font-bold text-5xl md:text-7xl lg:text-8xl tracking-tight text-gray-900 dark:text-zinc-100">Our Jewelries</h1>
       </div>
       <div className="container">
-        <div className="py-3 my-8 border-t-[1px] border-b-[1px] border-gray-300 flex flex-row justify-between mx-8 2xl:mx-0">
+        <div className="py-3 my-8 border-t-[1px] border-b-[1px] border-gray-500 flex flex-row justify-between mx-8 2xl:mx-0 dark:border-gray-400">
           <div className="flex">
-            <h2 className="mr-4 font-lato font-medium text-xl uppercase">Sort by</h2>
+            <h2 className="mr-4 font-lato font-medium text-xl text-gray-500 dark:text-gray-400 uppercase">Sort by</h2>
             {/* TODO: Add a select drop down for category filtering & maybe (?) a search bar */}
           </div>
           <div>
-            <h2 className="font-lato font-medium text-xl">{`${products.length} Products`}</h2>
+            <h2 className="font-lato font-medium text-xl text-gray-500 dark:text-gray-400">{`${products.length} Products`}</h2>
           </div>
         </div>
       </div>
@@ -50,17 +50,20 @@ const Products = () => {
         {products.map((product, productIndex) => {
           const { id, productCategory, productName, productPrice, productThumbnail } = product;
           return (
-            <div key={id} className="relative bg-white inline-block mx-auto mb-12 shadow-md">
+            <div key={id} className="relative bg-white inline-block mx-auto mb-12 shadow-md dark:bg-gray-800">
               <img src={productThumbnail} alt={productName} className="w-[18rem] h-[22rem] object-cover" />
               <div className="mx-4 my-4">
                 <div className="flex justify-between">
-                  <h2 className="text-xl font-playfair font-medium text-gray-900">{productName}</h2>
-                  <h2 className="bg-gray-100 px-4 text-sm my-auto rounded-2xl text-gray-400 cursor-pointer transition duration-300 hover:bg-gray-300 active:bg-gray-400 active:text-gray-500">{productCategory}</h2>
+                  <h2 className="text-xl font-playfair font-medium text-gray-900 dark:text-zinc-100">{productName}</h2>
+                  <h2 className="bg-gray-100 px-4 text-sm my-auto rounded-2xl text-gray-400 cursor-pointer transition duration-300 hover:bg-gray-300 active:bg-gray-400 active:text-gray-500 dark:bg-gray-700 dark:text-gray-500">{productCategory}</h2>
                 </div>
-                <h1 className="text-2xl font-lato font-bold text-gray-900">&#8369;{productPrice}</h1>
+                <h1 className="text-2xl font-lato font-bold text-gray-900 dark:text-zinc-100">&#8369;{productPrice}</h1>
                 <div className="flex justify-between mt-4">
-                  <BsCartPlus className="h-[1.6rem] w-[1.6rem] text-sky-300 my-auto cursor-pointer transition duration-300 hover:text-sky-500 active:text-sky-600" />
-                  <a href="#" className="flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium shadow-md transition duration-300 hover:shadow-2xl text-white bg-sky-300 hover:bg-sky-500 md:py-2 md:text-lg md:px-8 focus:ring-2 focus:ring-offset-2 focus:ring-sky-300 active:bg-sky-600">
+                  <BsCartPlus className="h-[1.6rem] w-[1.6rem] text-sky-300 my-auto cursor-pointer transition duration-300 hover:text-sky-500 active:text-sky-600 dark:text-sky-500" />
+                  <a
+                    href="#"
+                    className="flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium shadow-md transition duration-300 hover:shadow-2xl text-white bg-sky-300 hover:bg-sky-500 md:py-2 md:text-lg md:px-8 focus:ring-2 focus:ring-offset-2 focus:ring-sky-300 active:bg-sky-600"
+                  >
                     Buy Now
                   </a>
                 </div>
