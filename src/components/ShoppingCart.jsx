dd80835baff03b782a8ba5833f3efd14cfecc8b9
cedricquitor <cedricquitor.dev@gmail.com>
2022-02-17 11:45:03 +0800

    Finished cart design

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,27 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+import data from "../test/data";
 
 const ShoppingCart = () => {
+  const [products, setProducts] = useState(data);
+
   return (
     <div>
       <h1>ShoppingCart</h1>
-      <table className="mx-auto mt-8 table-auto">
-        <thead className="font-playfair">
-          <tr>
-            <th>Product</th>
-            <th>Unit Price</th>
-            <th>Quantity</th>
-            <th>Total Price</th>
-          </tr>
-        </thead>
-        <tbody className="font-lato">
-          <tr>
-            <td>Test Product</td>
-            <td>123</td>
-            <td>2</td>
-            <td>246</td>
-          </tr>
-        </tbody>
-      </table>
+      <div className="flex flex-col w-11/12 xl:w-10/12 2xl:w-2/3 mx-auto md:mt-12">
+        <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+              <table className="min-w-full divide-y divide-gray-200">
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                      Product
+                    </th>
+                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                      Unit Price
+                    </th>
+                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                      Quantity
+                    </th>
+                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                      Total Price
+                    </th>
+                    <th scope="col" className="relative px-6 py-3">
+                      <span className="sr-only">Edit</span>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="bg-white divide-y divide-gray-200">
+                  {products.map((product) => {
+                    const { id, productCategory, productName, productPrice, productThumbnail } = product;
+                    return (
+                      <tr key={product.productName}>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <div className="flex items-center">
+                            <div className="flex-shrink-0 h-10 w-10">
+                              <img className="h-10 w-10 rounded-full" src={product.productThumbnail} alt="" />
+                            </div>
+                            <div className="ml-4">
+                              <div className="text-sm font-medium text-gray-900">{product.productName}</div>
+                              <div className="text-sm text-gray-500">{product.productCategory}</div>
+                            </div>
+                          </div>
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <div className="text-sm text-gray-900">{product.productPrice}</div>
+                          <div className="text-sm text-gray-500">{product.productPrice}</div>
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">Active</span>
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{product.productFeatured}</td>
+                        <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                          <a href="#" className="text-indigo-600 hover:text-indigo-900">
+                            Edit
+                          </a>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
