6853d55d4fb63efb2539d4bdbd6fd213b5e4d113
cedricquitor <cedricquitor.dev@gmail.com>
2022-03-24 09:50:06 +0800

    Finished UI of add product in admin panel

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect, useRef } from "react";
 const AddProduct = () => {
   const handleSubmit = async (e) => {
     console.log(e);
+    e.preventDefault();
   };
 
   const [isFeatured, setIsFeatured] = useState(true);
@@ -28,6 +29,9 @@ const AddProduct = () => {
     <section className="mt-20 flex justify-center">
       <div className="mx-auto my-auto py-8 w-5/6 md:w-2/3 xl:w-1/3 2xl:w-1/4 bg-white shadow-lg dark:bg-gray-700">
         <div className="mx-8 md:mx-16 mb-4 md:mb-8">
+          <h1 className="text-center font-playfair text-gray-500 dark:text-zinc-100">
+            <span className="text-sky-300 dark:text-sky-500">Trinkets</span> of Luna
+          </h1>
           <h1 className="mb-2 font-playfair font-medium text-gray-900 text-2xl md:text-3xl text-center dark:text-zinc-100">Add Product</h1>
         </div>
         <div className="relative px-10">
@@ -56,27 +60,31 @@ const AddProduct = () => {
                 Product Thumbnail
               </label>
             </div>
-            <div className="mt-8">
-              <p>Is this product featured?</p>
+            <div className="mt-4">
+              <div className="flex flex-col mb-2">
+                <p className="font-lato text-gray-900 text-lg -mb-1 dark:text-zinc-100">Is this product featured?</p>
+                <span className="font-lato text-xs text-gray-500 dark:text-gray-400">Featured products are shown in sliders and carousels.</span>
+              </div>
               <div className="flex">
                 <div className="pr-2 w-full">
-                  <input className="hidden" id="radio_1" type="radio" name="radio" onChange={handleRadioChange} defaultChecked />
-                  <label className="flex flex-col p-4 border-2 border-gray-400 cursor-pointer" htmlFor="radio_1">
-                    <span className="text-xl font-bold font-playfair mt-2">Yes</span>
+                  <input className="hidden input__radio" id="radio_1" type="radio" name="radio" onChange={handleRadioChange} defaultChecked />
+                  <label className="flex flex-col p-2 border-2 border-gray-500 cursor-pointer transition duration-300 hover:border-sky-200 hover:shadow-md active:border-sky-500 dark:border-gray-400 md:p-3" htmlFor="radio_1">
+                    <span className="text-xl font-bold font-playfair mt-2 mx-auto">Yes</span>
                   </label>
                 </div>
                 <div className="pl-2 w-full">
-                  <input className="hidden" id="radio_2" type="radio" name="radio" onChange={handleRadioChange} />
-                  <label className="flex flex-col p-4 border-2 border-gray-400 cursor-pointer" htmlFor="radio_2">
-                    <span className="text-xl font-bold font-playfair mt-2">No</span>
+                  <input className="hidden input__radio" id="radio_2" type="radio" name="radio" onChange={handleRadioChange} />
+                  <label className="flex flex-col p-2 border-2 border-gray-500 cursor-pointer transition duration-300 hover:border-sky-200 hover:shadow-md active:border-sky-500 dark:border-gray-400 md:p-3" htmlFor="radio_2">
+                    <span className="text-xl font-bold font-playfair mt-2 mx-auto">No</span>
                   </label>
                 </div>
               </div>
             </div>
+            <button className="btn--primary mt-6 mx-auto">Add Product</button>
           </form>
         </div>
       </div>
-      <button onClick={() => console.log(isFeatured)}>Check</button>
+      <button onClick={() => console.log(isFeatured)}>Check isFeatured</button>
     </section>
   );
 };
