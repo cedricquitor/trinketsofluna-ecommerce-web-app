fd6c10e9654183f5eb6fc70b8360901be72a3d77
cedricquitor <cedricquitor.dev@gmail.com>
2022-04-07 16:50:23 +0800

    Cart now clears when checkout failed

--- src/components/CheckoutFailed.jsx
+++ src/components/CheckoutFailed.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import { RiErrorWarningFill } from "react-icons/ri";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { clearCartItems, clearPaymongoResponse } from "../redux/tempSlice";
+import { clearCart } from "../redux/cartSlice";
+import { useDispatch } from "react-redux";
 
 const CheckoutFailed = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const backToCart = () => {
+    dispatch(clearCartItems());
+    dispatch(clearPaymongoResponse());
+    dispatch(clearCart());
+    navigate("/", { replace: true });
+  };
+
   return (
     <section className="mt-36 flex justify-center">
       <div className="mx-auto my-auto py-8 w-5/6 md:w-2/3 xl:w-1/3 2xl:w-1/4 bg-white shadow-lg dark:bg-gray-700">
@@ -16,9 +29,9 @@ const CheckoutFailed = () => {
           <p className="font-lato text-xs text-center text-gray-500 dark:text-gray-400">The payment gateway only allows 5 minutes to complete your payment. The transaction will fail after the allocated time.</p>
         </div>
         <div className="flex">
-          <Link to="/cart" className="btn--secondary my-auto mx-auto py-3">
+          <button onClick={() => backToCart()} className="btn--secondary my-auto mx-auto py-3">
             Back to Cart
-          </Link>
+          </button>
         </div>
       </div>
     </section>
