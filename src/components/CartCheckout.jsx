5cdc614f492c22d80b7054c9dfd7acbf3e990d85
cedricquitor <cedricquitor.dev@gmail.com>
2022-05-08 11:12:38 +0800

    Changed redirect after checkout

--- src/components/CartCheckout.jsx
+++ src/components/CartCheckout.jsx
@@ -57,8 +57,8 @@ const CartCheckout = () => {
           attributes: {
             amount: standardizedPrice,
             redirect: {
-              success: "http://localhost:3000/checkout/success",
-              failed: "http://localhost:3000/checkout/failed",
+              success: "https://trinketsofluna-ecommerce-fbase.web.app/checkout/success",
+              failed: "https://trinketsofluna-ecommerce-fbase.web.app/checkout/failed",
             },
             billing: { address: { line1: `${addressRef.current.value}`, city: `${cityRef.current.value}` }, name: `${nameRef.current.value}`, phone: `${phoneNumberRef.current.value}`, email: auth.currentUser.email },
             type: "gcash",
